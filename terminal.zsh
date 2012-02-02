4fa84266caa6b688f42446e91b01586afce7434e
Serhiy Oplakanets <serhiy@oplakanets.com>
2012-02-02 14:51:11 +0100

    Initial version.

---
+++ terminal.zsh
@@ -0,0 +1,11 @@
+case $TERM in
+  xterm*|rxvt)
+    precmd () { print -Pn "\e]0;%n@%m: %~\a" }
+    preexec () { print -Pn "\e]0;%n@%m: $1\a" }
+    ;;
+  screen)
+    precmd () { print -Pn "\033k%~\033\\" }
+    preexec () { print -Pn "\033k$1\033\\" }
+    ;;
+esac
+
